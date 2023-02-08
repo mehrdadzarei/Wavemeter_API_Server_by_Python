3b4d5bcc25cc8adf0956f5414a69f4007d0c7b9f
Mehrdad <mehr.zarei1@gmail.com>
2023-02-08 15:19:28 +0100

    modify reconnection of digilock

--- run_testDigiLock.bat
+++ run_testDigiLock.bat
@@ -3,13 +3,13 @@
 @echo off
 
 @REM set path of activeate.bat file
-call C:\Users\srcal\anaconda3\Scripts\activate.bat
+call C:\Users\RydbergLaser\Anaconda3\Scripts\activate.bat
 
 @REM set the path which you save the testDigiLock.py file
-cd C:\Users\srcal\Desktop\mehrdad\Wavemeter_API_Server_by_Python
+cd C:\Users\RydbergLaser\Desktop\Mehrdad\Wavemeter_API_Server_by_Python
 
 @REM args: IP(DigiLock) Port(DigiLock)
-python testDigiLock.py "192.168.0.175" 60001
+python testDigiLock.py "192.168.0.175" 60001 0.03
 
 
 @pause
--- testDigiLock.py
+++ testDigiLock.py
@@ -16,10 +16,12 @@ import matplotlib.pyplot as plt
 
 
 digi = DigiLock.digiClient()
-digi.connect(ip = str(sys.argv[1]), port = int(sys.argv[2]))
+digi_ip = str(sys.argv[1])
+digi_port = int(sys.argv[2])
+digi.connect(ip = digi_ip, port = digi_port)
 
 # peak to peak level which you can measure from amplitude on DigiLock
-digi.set_peakTpeak(ptp = 0.04)
+digi.set_peakTpeak(ptp = float(sys.argv[3]))
 
 err_con = digi.setting()
 err_con, err = digi.checking()
@@ -32,20 +34,31 @@ if err_con == 1 and err == 1:
         err_con, err = digi.lock()
 
 up_t = 1
+t1 = time.time()
 while err_con == 1:
 
+    # refresh digilock connection every 100 s to avoid freezing
+    if (time.time() - t1) > 99:
+
+        digi.__del__()
+        time.sleep(1)
+        digi_con = digi.connect(ip = digi_ip, port = digi_port)
+        
+        t1 = time.time()    # restart timing
+            
+    
     time.sleep(up_t)
     err_con, err, up_t = digi.check_lock()
 
 
 
-x, y = digi.get_graph()
-plt.plot(x, y, label='data')
+# x, y = digi.get_graph()
+# plt.plot(x, y, label='data')
 # plt.title('Life time of Sr atoms in the Lattice', fontdict=self.font)
 # plt.legend()
-plt.xlabel('SC110 out [V]', fontdict=digi.font)
-plt.ylabel('Main in [V]', fontdict=digi.font)
-plt.show()
+# plt.xlabel('SC110 out [V]', fontdict=digi.font)
+# plt.ylabel('Main in [V]', fontdict=digi.font)
+# plt.show()
 
 
 digi.__del__()
